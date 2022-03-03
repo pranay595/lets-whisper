6f6696f9da6a73ad71f93ace42aae0df998a7c67
pranay lohe <pranaylohe2077@gmail.com>
2022-03-03 20:15:50 +0530

    Add hashing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const md5 =  require("md5");
+
 var encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -11,15 +13,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
 app.set("view engine", "ejs");
 
-
-
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
 
 userSchema.plugin(encrypt, { secret: process.env.API_KEY+process.env.SECRET, encryptedFields: ["password"]});
 
@@ -39,7 +38,7 @@ app.get("/register",(req,res)=>{
 
 app.post("/register",(req,res)=>{
     const userEmail = req.body.username;
-    const userPassword = req.body.password;
+    const userPassword = md5(req.body.password);
     const regiseredUser = new User({
         email: userEmail,
         password: userPassword
@@ -54,7 +53,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const userEmail = req.body.username;
-    const userPassword = req.body.password;
+    const userPassword = md5(req.body.password);
     User.findOne({email: userEmail},(err,foundUser)=>{
         if(err)
         console.log("Credintials did not match with our dataset");
@@ -66,6 +65,7 @@ app.post("/login",(req,res)=>{
         }
     })
 })
+console.log(md5("12345"));
 
 app.listen(PORT, ()=>{
     console.log("Server is running on port: "+PORT);
